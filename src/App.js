7bd75d590c2e5c29a48d329a7ae904384f205b01
Mickael Riss <mendos.prod@gmail.com>
2023-10-01 01:39:19 +0200

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/Navbar';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [burgerMenu, setBurgerMenu] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setBurgerMenu(true);
+      }
+      else {
+        setBurgerMenu(false);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, [burgerMenu]);
+
   return (
     <div>
-      <Navbar />
+      <Navbar burgerMenu={burgerMenu} />
       <div className="container">
         <p>Hello</p>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,51 @@
-import './Navbar.css';
+import './Navbar.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShuttleSpace } from '@fortawesome/free-solid-svg-icons';
+import { faShuttleSpace, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-function Navbar() {
+function Navbar({ burgerMenu }) {
+  const [showNav, setShowNav] = useState(false);
+  const handleShowBar = () => {
+    setShowNav(!showNav);
+  }
+
+  const navMenuClasses = showNav ? 'nav-menu-show show-nav-menu' : 'nav-menu-show';
+
   return (
+    burgerMenu ? (
+      <div className="navbar-container">
+        <div className="nav-title">
+          <h1>Space Travel</h1>
+          <FontAwesomeIcon icon={faShuttleSpace} rotation={270} size="xl" style={{color: "#ffffff",}} />
+        </div>
+        { showNav ? (
+          <FontAwesomeIcon onClick={handleShowBar} icon={faXmark} size="xl" style={{color: "#ffffff", cursor: "pointer",}} />
+          ) : (
+            <FontAwesomeIcon onClick={handleShowBar} icon={faBars} size="xl" style={{color: "#ffffff", cursor: "pointer",}}/>
+          )}
+        {showNav &&
+          <div className={navMenuClasses}>
+            <Link to="/" className='nav-item'>Home</Link>
+            <Link to="/Services" className='nav-item'>Services</Link>
+            <Link to="/about" className='nav-item'>About</Link>
+            <Link to="/signup" className='signup'>Sign Up</Link>
+          </div>
+        }
+      </div>
+    ) :
     <div className="navbar-container">
       <div className="nav-title">
         <h1>Space Travel</h1>
         <FontAwesomeIcon icon={faShuttleSpace} rotation={270} size="xl" style={{color: "#ffffff",}} />
       </div>
-      <div className="nav-links">
+      <div className='nav-links-container'>
+        <div className="nav-links">
+          <Link to="/" className='nav-item'>Home</Link>
+          <Link to="/Services" className='nav-item'>Services</Link>
+          <Link to="/about" className='nav-item'>About</Link>
+        </div>
+        <Link to="/signup" className='signup'>Sign Up</Link>
       </div>
     </div>
   )
